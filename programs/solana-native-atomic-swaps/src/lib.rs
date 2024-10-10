5652eb6a87fa4f960503f3d1a409fedb85da8431
Shiva Kiran K <shiva_kr@riseup.net>
2024-10-10 15:32:19 +0530

    Initial commit

---
+++ programs/solana-native-atomic-swaps/src/lib.rs
@@ -0,0 +1,168 @@
+use anchor_lang::prelude::*;
+use anchor_lang::system_program;
+use solana_program::hash;
+
+declare_id!("GmmfyjGJaSaH7ZgRp4BYEr7v9vydtd4NKg4yJnWdzRgj");
+
+type Lamports = u64; // 1 SOL = 10^9 lamports
+type Slots = u64; // 1 slot = 400ms
+
+#[program]
+pub mod solana_native_atomic_swaps {
+    use super::*;
+
+    pub fn initiate(
+        ctx: Context<Initiate>,
+        redeemer: Pubkey,
+        secret_hash: [u8; 32],
+        amount: Lamports,
+        expires_in: Slots,
+    ) -> Result<()> {
+        let swap_id =
+            hash::hash(&[ctx.accounts.initiator.key().as_ref(), &secret_hash].concat()).to_bytes();
+        *ctx.accounts.swap_account = SwapAccount {
+            swap_id,
+            redeemer,
+            secret_hash,
+            amount,
+            initiator: ctx.accounts.initiator.key(),
+            expiry_slot: Clock::get()?.slot + expires_in,
+        };
+
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.initiator.to_account_info(),
+                to: ctx.accounts.swap_account.to_account_info(),
+            },
+        );
+        system_program::transfer(cpi_context, amount)?;
+        emit!(Initiated { swap_id, secret_hash, amount });
+
+        Ok(())
+    }
+
+    pub fn redeem(ctx: Context<Redeem>, secret: [u8; 32]) -> Result<()> {
+        let SwapAccount { swap_id, secret_hash, .. } = *ctx.accounts.swap_account;
+        require!(hash::hash(&secret).to_bytes() == secret_hash, SwapError::InvalidSecret);
+        emit!(Redeemed { swap_id, secret });
+        // All SOL in the swap account incl. rent fees will be transferred to the redeemer
+        // by the 'close' attribute in the Redeem struct
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        let SwapAccount { swap_id, expiry_slot, .. } = *ctx.accounts.swap_account;
+        let current_slot = Clock::get()?.slot;
+        require!(current_slot >= expiry_slot, SwapError::RefundBeforeExpiry);
+        emit!(Refunded { swap_id });
+        // All SOL in the swap account incl. rent fees will be transferred to the refundee
+        // by the 'close' attribute in the Refund struct
+        Ok(())
+    }
+
+    pub fn instant_refund(ctx: Context<InstantRefund>) -> Result<()> {
+        emit!(RefundedInstantly { swap_id: ctx.accounts.swap_account.swap_id });
+        // All SOL in the swap account incl. rent fees will be transferred to the refundee
+        // by the 'close' attribute in the RefundInstant struct
+        Ok(())
+    }
+}
+
+#[account]
+pub struct SwapAccount {
+    swap_id: [u8; 32],
+    initiator: Pubkey,
+    redeemer: Pubkey,
+    secret_hash: [u8; 32],
+    expiry_slot: u64,
+    amount: Lamports,
+}
+
+#[derive(Accounts)]
+// redeemer is included in #[instruction(...)] for reasons outlined in:
+// https://www.anchor-lang.com/docs/account-constraints#instruction-attribute
+#[instruction(redeemer: Pubkey, secret_hash: [u8; 32])]
+pub struct Initiate<'info> {
+    #[account(
+        init,
+        payer = initiator,
+        seeds = [b"swap_account".as_ref(), initiator.key().as_ref(), secret_hash.as_ref()],
+        bump,
+        space = 8 + std::mem::size_of::<SwapAccount>(),
+    )]
+    pub swap_account: Account<'info, SwapAccount>,
+
+    #[account(mut)]
+    pub initiator: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Redeem<'info> {
+    #[account(mut, close = redeemer)] // Closes and transfers all SOL to redeemer upon success
+    pub swap_account: Account<'info, SwapAccount>,
+
+    /// CHECK: The public key of the redeemer
+    #[account(mut, address = swap_account.redeemer @ SwapError::InvalidRedeemer)]
+    pub redeemer: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Refund<'info> {
+    #[account(mut, close = refundee)] // Closes and transfers all SOL to refundee upon success
+    pub swap_account: Account<'info, SwapAccount>,
+
+    /// CHECK: The public key of the initiator
+    #[account(mut, address = swap_account.initiator @ SwapError::InvalidRefundee)]
+    pub refundee: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct InstantRefund<'info> {
+    #[account(mut, close = initiator)]
+    // Closes and transfers all SOL to the initiator upon success
+    pub swap_account: Account<'info, SwapAccount>,
+
+    #[account(mut, address = swap_account.initiator @ SwapError::InvalidRefundee)]
+    pub initiator: Signer<'info>,
+
+    #[account(address = swap_account.redeemer @ SwapError::InvalidRedeemer)]
+    pub redeemer: Signer<'info>,
+}
+
+#[event]
+pub struct Initiated {
+    swap_id: [u8; 32],
+    secret_hash: [u8; 32],
+    amount: u64,
+}
+#[event]
+pub struct Redeemed {
+    pub swap_id: [u8; 32],
+    pub secret: [u8; 32],
+}
+#[event]
+pub struct Refunded {
+    pub swap_id: [u8; 32],
+}
+#[event]
+pub struct RefundedInstantly {
+    pub swap_id: [u8; 32],
+}
+
+#[error_code]
+pub enum SwapError {
+    #[msg("The provided redeemer is not the intended recipient of the swap amount")]
+    InvalidRedeemer,
+
+    #[msg("The provided refundee is not the initiator of the given swap account")]
+    InvalidRefundee,
+
+    #[msg("The provided secret does not correspond to the secret hash in the PDA")]
+    InvalidSecret,
+
+    #[msg("Attempt to perform a refund before expiry time")]
+    RefundBeforeExpiry,
+}
---
+++ tests/solana-native-atomic-swaps.ts
@@ -0,0 +1,119 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaNativeAtomicSwaps } from "../target/types/solana_native_atomic_swaps";
+
+import * as crypto from 'crypto';
+import { expect } from "chai";
+
+// Configure the client to use the local cluster.
+anchor.setProvider(anchor.AnchorProvider.env());
+const connection = anchor.getProvider().connection;
+const program = anchor.workspace.SolanaNativeAtomicSwaps as Program<SolanaNativeAtomicSwaps>;
+const LAMPORTS_PER_SOL = anchor.web3.LAMPORTS_PER_SOL;
+const MILLIS_PER_SLOT = 400;
+
+describe("Testing one way swap between Alice and Bob", () => {
+    const swapAmount = new anchor.BN(0.1 * LAMPORTS_PER_SOL);
+    const swapExpiresIn = new anchor.BN(1000 / MILLIS_PER_SLOT); // 1 second
+
+    // Alice is the initiator here
+    // Alice's stuff
+    const alice = new anchor.web3.Keypair();
+    const alicePubkey = alice.publicKey;
+    const secret = Buffer.from(Array(32).fill(0));
+    const secretHash = [...(crypto.createHash('sha256').update(secret).digest())];
+
+    // Bob's stuff
+    const bob = new anchor.web3.Keypair();
+    const bobPubkey = bob.publicKey;
+
+    // PDA
+    const pdaSeeds = [Buffer.from("swap_account"), alicePubkey.toBuffer(), Buffer.from(secretHash)];
+    const [swapAccount,] = anchor.web3.PublicKey.findProgramAddressSync(pdaSeeds, program.programId);
+    const size = program.account.swapAccount.size;
+    let rentAmount: number;
+
+    console.log(`Alice: ${alicePubkey}\nBob: ${bobPubkey}\nSwap Account (PDA): ${swapAccount}`);
+
+    const aliceInitiate = () => new Promise<void>(async resolve => {
+        await program.methods.initiate(bobPubkey, secretHash, swapAmount, swapExpiresIn)
+            .accounts({
+                swapAccount,
+                initiator: alicePubkey,
+            }).signers([alice]).rpc()
+            .then(async sig => {
+                console.log("Alice initiated with Signature:", sig);
+                await connection.confirmTransaction(sig);
+            });
+            resolve();
+        }
+    );
+
+    before(async () => {
+        // Fund alice's wallet with 1 SOL
+        await connection.requestAirdrop(alicePubkey, anchor.web3.LAMPORTS_PER_SOL)
+        .then(async sig => await connection.confirmTransaction(sig));
+        // For some program sizes, larger rentAmount (e.g. six bytes more worth) is taken for some reason
+        rentAmount = await connection.getMinimumBalanceForRentExemption(size);
+    });
+
+    it("Test initiation", async () => {
+        await aliceInitiate();
+        const pdaBalance = await connection.getBalance(swapAccount);
+        expect(pdaBalance - rentAmount).to.equal(swapAmount.toNumber());
+
+    });
+
+    it("Test redeem", async () => {
+        // The previous test has already initiated the swap
+        await program.methods.redeem([...secret])
+        .accounts({
+            swapAccount,
+            redeemer: bobPubkey,
+        }).rpc()
+        .then(async sig => {
+            console.log("Bob redeemed with Signature:", sig);
+            await connection.confirmTransaction(sig);
+        });
+
+        const pdaBalance = await connection.getBalance(swapAccount);
+        expect(pdaBalance).to.equal(0);
+    });
+
+    it("Test refund", async () => {
+        await aliceInitiate();  // Re-initiating for the sake of testcase
+
+        console.log("Awaiting timelock for refund");
+        await new Promise(r => setTimeout(r, swapExpiresIn.toNumber() * MILLIS_PER_SLOT));
+        await program.methods.refund()
+        .accounts({
+            swapAccount,
+            refundee: alicePubkey,
+        }).rpc()
+        .then(async sig => {
+            console.log("Alice refunded with Signature:", sig);
+            await connection.confirmTransaction(sig);
+        });
+
+        const pdaBalance = await connection.getBalance(swapAccount);
+        expect(pdaBalance).to.equal(0);
+    });
+
+    it("Test instant refund", async () => {
+        await aliceInitiate();  // Re-initiating for the sake of testcase
+        await program.methods.instantRefund()
+        .accounts({
+            swapAccount,
+            initiator: alicePubkey,
+            redeemer: bobPubkey,
+        }).signers([alice, bob])
+        .rpc()
+        .then(async sig => {
+            console.log("Alice instant-refunded with Signature:", sig);
+            await connection.confirmTransaction(sig);
+        });
+
+        const pdaBalance = await connection.getBalance(swapAccount);
+        expect(pdaBalance).to.equal(0);
+    });
+});
