4a337247c98968b877a52ad70f3858f91a004552
Shiva Kiran K <shiva_kr@riseup.net>
2025-04-25 11:51:27 +0530

    fix: upgrade contract interface, emit all params on init for off-chain verification
    initiate event now emits all swap params, which need to be verified
    off-chain for security.
    similarly, refund and instant refund events emit the secret hash
    instead of swap id (it has been totally decoupled from the contract).
    this secret hash can be used as the unique identifier for the swap.
    even the PDA is now derived from the secret hash.

--- tests/solana-native-atomic-swaps.ts
+++ tests/solana-native-atomic-swaps.ts
@@ -13,110 +13,107 @@ const LAMPORTS_PER_SOL = anchor.web3.LAMPORTS_PER_SOL;
 const MILLIS_PER_SLOT = 400;
 
 describe("Testing one way swap between Alice and Bob", () => {
-    const swapAmount = new anchor.BN(0.1 * LAMPORTS_PER_SOL);
-    const swapExpiresIn = 1000 / MILLIS_PER_SLOT; // 1 second
+	const swapAmount = new anchor.BN(0.1 * LAMPORTS_PER_SOL);
+	const expiresInSlots = new anchor.BN(800 / MILLIS_PER_SLOT); // 0.8 secs
 
-    // Alice is the initiator here
-    // Alice's stuff
-    const alice = new anchor.web3.Keypair();
-    const alicePubkey = alice.publicKey;
-    const secret = Buffer.from(Array(32).fill(0));
-    const secretHash = [...(crypto.createHash('sha256').update(secret).digest())];
+	// Alice is the initiator here
+	// Alice's stuff
+	const alice = anchor.web3.Keypair.fromSeed(new Uint8Array(32).fill(0));
+	const secret = Buffer.from(Array(32).fill(0));
+	const secretHash = [...(crypto.createHash('sha256').update(secret).digest())];
 
-    const _swapIdPreImage = Buffer.concat([alicePubkey.toBuffer(), Buffer.from(secretHash)]);
-    const swapId = crypto.createHash('sha256').update(_swapIdPreImage).digest();
+	// Bob's stuff
+	const bob = anchor.web3.Keypair.fromSeed(new Uint8Array(32).fill(1));
 
-    // Bob's stuff
-    const bob = new anchor.web3.Keypair();
-    const bobPubkey = bob.publicKey;
+	// PDA
+	const pdaSeeds = [Buffer.from("swap_account"), Buffer.from(secretHash)];
+	const [swapAccount,] = anchor.web3.PublicKey.findProgramAddressSync(pdaSeeds, program.programId);
+	const size = program.account.swapAccount.size;
+	let rentAmount: number;
 
-    // PDA
-    const pdaSeeds = [Buffer.from("swap_account"), swapId];
-    const [swapAccount,] = anchor.web3.PublicKey.findProgramAddressSync(pdaSeeds, program.programId);
-    const size = program.account.swapAccount.size;
-    let rentAmount: number;
+	console.log(`Alice: ${alice.publicKey}\nBob: ${bob.publicKey}\nSwap Account (PDA): ${swapAccount}`);
 
-    console.log(`Alice: ${alicePubkey}\nBob: ${bobPubkey}\nSwap Account (PDA): ${swapAccount}`);
-
-    const aliceInitiate = () => new Promise<void>(async resolve => {
-        await program.methods.initiate([...swapId], bobPubkey, secretHash, swapAmount, swapExpiresIn)
-            .accounts({
-                initiator: alicePubkey,
-            }).signers([alice]).rpc()
-            .then(async signature => {
-                console.log("Alice initiated with Signature:", signature);
-                await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-            });
-            resolve();
+	const aliceInitiate = () => new Promise<void>(async resolve => {
+		console.log("alice is initiating");
+		await program.methods.initiate(swapAmount, expiresInSlots, bob.publicKey, secretHash)
+			.accounts({
+				initiator: alice.publicKey,
+			}).signers([alice]).rpc()
+			.then(async signature => {
+				console.log("Alice initiated with Signature:", signature);
+				await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
+			});
+		resolve();
         }
-    );
+	);
 
-    before(async () => {
-        // Fund alice's wallet with 1 SOL
-        await connection.requestAirdrop(alicePubkey, anchor.web3.LAMPORTS_PER_SOL)
-        .then(async signature =>
-            // For some program sizes, larger rentAmount (e.g. six bytes more worth) is taken somehow
-            await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())}));
-        rentAmount = await connection.getMinimumBalanceForRentExemption(size);
-    });
+	before(async () => {
+		console.log("performing airdrop of 1 SOL to alice");
+		// Fund alice's wallet with 1 SOL
+		await connection.requestAirdrop(alice.publicKey, anchor.web3.LAMPORTS_PER_SOL)
+			.then(async signature =>
+				// For some program sizes, larger rentAmount (e.g. six bytes more worth) is taken somehow
+				await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())}));
+		console.log("airdrop successful");
+		rentAmount = await connection.getMinimumBalanceForRentExemption(size);
+	});
 
-    it("Test initiation", async () => {
-        await aliceInitiate();
-        const pdaBalance = await connection.getBalance(swapAccount);
-        expect(pdaBalance - rentAmount).to.equal(swapAmount.toNumber());
+	it("Test initiation", async () => {
+		await aliceInitiate();
+		const pdaBalance = await connection.getBalance(swapAccount);
+		expect(pdaBalance - rentAmount).to.equal(swapAmount.toNumber());
+	});
 
-    });
+	it("Test redeem", async () => {
+		// The previous test has already initiated the swap
+		await program.methods.redeem([...secret])
+			.accounts({
+				swapAccount,
+				redeemer: bob.publicKey,
+			}).rpc()
+			.then(async signature => {
+				console.log("Bob redeemed with Signature:", signature);
+				await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
+			});
 
-    it("Test redeem", async () => {
-        // The previous test has already initiated the swap
-        await program.methods.redeem([...secret])
-        .accounts({
-            swapAccount,
-            redeemer: bobPubkey,
-        }).rpc()
-        .then(async signature => {
-            console.log("Bob redeemed with Signature:", signature);
-            await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-        });
+		const pdaBalance = await connection.getBalance(swapAccount);
+		expect(pdaBalance).to.equal(0);
+	});
 
-        const pdaBalance = await connection.getBalance(swapAccount);
-        expect(pdaBalance).to.equal(0);
-    });
+	it("Test refund", async () => {
+		await aliceInitiate();  // Re-initiating for the sake of testcase
 
-    it("Test refund", async () => {
-        await aliceInitiate();  // Re-initiating for the sake of testcase
+		console.log("Awaiting timelock for refund");
+		await new Promise(r => setTimeout(r, (expiresInSlots.toNumber() + 1) * MILLIS_PER_SLOT));
+		await program.methods.refund()
+			.accounts({
+				swapAccount,
+				refundee: alice.publicKey,
+			}).rpc()
+			.then(async signature => {
+				console.log("Alice refunded with Signature:", signature);
+				await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
+			});
 
-        console.log("Awaiting timelock for refund");
-        await new Promise(r => setTimeout(r, swapExpiresIn * MILLIS_PER_SLOT));
-        await program.methods.refund()
-        .accounts({
-            swapAccount,
-            refundee: alicePubkey,
-        }).rpc()
-        .then(async signature => {
-            console.log("Alice refunded with Signature:", signature);
-            await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-        });
+		const pdaBalance = await connection.getBalance(swapAccount);
+		expect(pdaBalance).to.equal(0);
+	});
 
-        const pdaBalance = await connection.getBalance(swapAccount);
-        expect(pdaBalance).to.equal(0);
-    });
+	it("Test instant refund", async () => {
+		await aliceInitiate();  // Re-initiating for the sake of testcase
+		await program.methods.instantRefund()
+			.accounts({
+				swapAccount,
+				initiator: alice.publicKey,
+				redeemer: bob.publicKey,
+			}).signers([bob])
+			.rpc()
+			.then(async signature => {
+				console.log("Alice instant-refunded with Signature:", signature);
+				await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
+			});
 
-    it("Test instant refund", async () => {
-        await aliceInitiate();  // Re-initiating for the sake of testcase
-        await program.methods.instantRefund()
-        .accounts({
-            swapAccount,
-            initiator: alicePubkey,
-            redeemer: bobPubkey,
-        }).signers([bob])
-        .rpc()
-        .then(async signature => {
-            console.log("Alice instant-refunded with Signature:", signature);
-            await connection.confirmTransaction({signature, ...(await connection.getLatestBlockhash())});
-        });
-
-        const pdaBalance = await connection.getBalance(swapAccount);
-        expect(pdaBalance).to.equal(0);
-    });
+		const pdaBalance = await connection.getBalance(swapAccount);
+		expect(pdaBalance).to.equal(0);
+	});
 });
