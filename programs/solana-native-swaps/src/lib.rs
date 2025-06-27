2db1b8c1baf9c95e2570f2d4673ecf9601ddd72e
Shiva Kiran K <142958357+skr4n@users.noreply.github.com>
2025-06-27 08:48:15 +0000

    Standardized event structure, doc comments and minor refactors (#4)
    * refactor: Rename `Initiated` event structure to match with SPL
    
    This is to have a common events structure for both native program and
    spl program.
    
    * refactor: Replace literal with const `ANCHOR_DISCRIMINATOR`
    
    * refactor: Use anchor provided `InitSpace` for specifying PDA size
    
    Anchor now provides an attribute macro `InitSpace` that adds a
    constant `INIT_SPACE` which specifies the size that needs to be
    allocated for a PDA during creation.
    Thus, use this instead of `std::mem::size_of`
    
    * fix: Initialize the on-chain swap data after the transfer
    
    The transfer instruction is a fallible operation which can fail
    e.g when the user doesn't have enough balance.
    Initializing the swap data after this operation would save
    compute during such cases.
    
    * refactor: Minor simplification to error strings
    
    * doc: Add documentation comments

--- programs/solana-native-swaps/src/lib.rs
+++ programs/solana-native-swaps/src/lib.rs
@@ -2,10 +2,20 @@ use anchor_lang::{prelude::*, solana_program::hash, system_program};
 
 declare_id!("2bag6xpshpvPe7SJ9nSDLHpxqhEAoHPGpEkjNSv7gxoF");
 
+/// The size of Anchor's internal discriminator in a PDA's memory
+const ANCHOR_DISCRIMINATOR: usize = 8;
+
 #[program]
 pub mod solana_native_swaps {
     use super::*;
 
+    /// Initiates the atomic swap. Funds are transferred from the initiator to the token vault.
+    /// As such, the initiator's signature is required for this instruction.
+    /// `amount_lamports` represents the quantity of native SOL to be transferred
+    /// through this atomic swap in base units (aka lamports).  
+    /// E.g: A quantity of 1 SOL must be provided as 1,000,000,000.
+    /// `expires_in_slots` represents the number of slots (1 slot = 400ms) after
+    /// which (non-instant) refunds are allowed.
     pub fn initiate(
         ctx: Context<Initiate>,
         amount_lamports: u64,
@@ -13,14 +23,6 @@ pub mod solana_native_swaps {
         redeemer: Pubkey,
         secret_hash: [u8; 32],
     ) -> Result<()> {
-        *ctx.accounts.swap_account = SwapAccount {
-            amount_lamports,
-            expiry_slot: Clock::get()?.slot + expires_in_slots,
-            initiator: ctx.accounts.initiator.key(),
-            redeemer,
-            secret_hash,
-        };
-
         let transfer_context = CpiContext::new(
             ctx.accounts.system_program.to_account_info(),
             system_program::Transfer {
@@ -30,8 +32,16 @@ pub mod solana_native_swaps {
         );
         system_program::transfer(transfer_context, amount_lamports)?;
 
-        emit!(Initiated {
+        *ctx.accounts.swap_account = SwapAccount {
             amount_lamports,
+            expiry_slot: Clock::get()?.slot + expires_in_slots,
+            initiator: ctx.accounts.initiator.key(),
+            redeemer,
+            secret_hash,
+        };
+
+        emit!(Initiated {
+            swap_amount: amount_lamports,
             expires_in_slots,
             initiator: ctx.accounts.initiator.key(),
             redeemer,
@@ -41,6 +51,7 @@ pub mod solana_native_swaps {
         Ok(())
     }
 
+    /// Funds are transferred to the redeemer. This instruction does not require any signatures.
     pub fn redeem(ctx: Context<Redeem>, secret: [u8; 32]) -> Result<()> {
         require!(
             hash::hash(&secret).to_bytes() == ctx.accounts.swap_account.secret_hash,
@@ -59,6 +70,9 @@ pub mod solana_native_swaps {
         Ok(())
     }
 
+    /// Funds are returned to the initiator, given that no redeems have occured
+    /// and the expiry slot has been reached.
+    /// This instruction does not require any signatures.
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
         let expiry_slot = ctx.accounts.swap_account.expiry_slot;
         let current_slot = Clock::get()?.slot;
@@ -76,6 +90,9 @@ pub mod solana_native_swaps {
         Ok(())
     }
 
+    /// Funds are returned to the initiator, with the redeemer's consent.
+    /// As such, the redeemer's signature is required for this instruction.
+    /// This allows for refunds before the expiry slot.
     pub fn instant_refund(ctx: Context<InstantRefund>) -> Result<()> {
         let swap_amount = ctx.accounts.swap_account.amount_lamports;
         ctx.accounts.swap_account.sub_lamports(swap_amount)?;
@@ -90,29 +107,43 @@ pub mod solana_native_swaps {
     }
 }
 
+/// Stores the state information of the atomic swap on-chain
 #[account]
+#[derive(InitSpace)]
 pub struct SwapAccount {
+    /// The quantity of native SOL to be transferred through this atomic swap in base units (aka lamports)
     amount_lamports: u64,
+    /// The exact slot after which (non-instant) refunds are allowed
     expiry_slot: u64,
+    /// The initiator of the atomic swap
     initiator: Pubkey,
+    /// The redeemer of the atomic swap
     redeemer: Pubkey,
+    /// The secret hash associated with the atomic swap
     secret_hash: [u8; 32],
 }
 
 #[derive(Accounts)]
-// https://www.anchor-lang.com/docs/account-constraints#instruction-attribute
+// The parameters must have the exact name and order as specified in the underlying function
+// to avoid "seed constraint violation" errors.
+// Refer: https://www.anchor-lang.com/docs/references/account-constraints#instruction-attribute
 #[instruction(amount_lamports: u64, expires_in_slots: u64, redeemer: Pubkey, secret_hash: [u8; 32])]
 pub struct Initiate<'info> {
+    /// A PDA that maintains the on-chain state of the atomic swap throughout its lifecycle.
+    /// It also serves as the "vault" for this swap, by escrowing the SOL involved in this swap.
+    /// The choice of seeds ensures that any swap with equal `initiator` and
+    /// `secret_hash` cannot be created until an existing one completes.
+    /// This PDA will be deleted upon completion of the swap.
     #[account(
         init,
         payer = initiator,
         seeds = [b"swap_account", initiator.key().as_ref(), &secret_hash],
         bump,
-        space = 8 + std::mem::size_of::<SwapAccount>(),
+        space = ANCHOR_DISCRIMINATOR + SwapAccount::INIT_SPACE,
     )]
     pub swap_account: Account<'info, SwapAccount>,
 
-    /// Initiator must sign this transaction
+    /// The initiator of the atomic swap. They must sign this transaction.
     #[account(mut)]
     pub initiator: Signer<'info>,
 
@@ -121,11 +152,14 @@ pub struct Initiate<'info> {
 
 #[derive(Accounts)]
 pub struct Redeem<'info> {
+    /// The PDA holding the state information of the atomic swap.
+    /// Will be closed upon successful execution and the resulting rent
+    /// will be transferred to the initiator.
     #[account(mut, close = initiator)]
     pub swap_account: Account<'info, SwapAccount>,
 
     /// CHECK: Verifying the initiator.  
-    /// This is included here for the PDA rent refund using the `close` attribute above
+    /// This is included here for the PDA rent refund using the `close` attribute above.
     #[account(mut, address = swap_account.initiator @ SwapError::InvalidInitiator)]
     pub initiator: AccountInfo<'info>,
 
@@ -136,47 +170,63 @@ pub struct Redeem<'info> {
 
 #[derive(Accounts)]
 pub struct Refund<'info> {
+    /// The PDA holding the state information of the atomic swap.
+    /// Will be closed upon successful execution and the resulting rent
+    /// will be transferred to the initiator.
     #[account(mut, close = initiator)]
     pub swap_account: Account<'info, SwapAccount>,
 
-    /// CHECK: Verifying the initiator  
+    /// CHECK: Verifying the initiator.
+    /// This is included here for the PDA rent refund using the `close` attribute above.
     #[account(mut, address = swap_account.initiator @ SwapError::InvalidInitiator)]
     pub initiator: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
 pub struct InstantRefund<'info> {
+    /// The PDA holding the state information of the atomic swap.
+    /// Will be closed upon successful execution and the resulting rent
+    /// will be transferred to the initiator.
     #[account(mut, close = initiator)]
     pub swap_account: Account<'info, SwapAccount>,
 
-    /// CHECK: Verifying the initiator
+    /// CHECK: Verifying the initiator.
+    /// This is included here for the PDA rent refund using the `close` attribute above.
     #[account(mut, address = swap_account.initiator @ SwapError::InvalidInitiator)]
     pub initiator: AccountInfo<'info>,
 
-    /// CHECK: Verifying the redeemer.  
-    /// Redeemer must sign this transaction
+    /// CHECK: Verifying the redeemer. Redeemer must sign this transaction.
     #[account(address = swap_account.redeemer @ SwapError::InvalidRedeemer)]
     pub redeemer: Signer<'info>,
 }
 
+/// Represents the initiated state of the swap where the initiator has deposited funds into the vault
 #[event]
 pub struct Initiated {
-    pub amount_lamports: u64,
+    /// The quantity of native SOL transferred through this atomic swap in base units (aka lamports).  
+    /// E.g: A quantity of 1 SOL will be represented as 1,000,000,000.
+    pub swap_amount: u64,
+    /// `expires_in_slots` represents the number of slots (1 slot = 400ms) after which
+    /// (non-instant) refunds are allowed
     pub expires_in_slots: u64,
     pub initiator: Pubkey,
     pub redeemer: Pubkey,
     pub secret_hash: [u8; 32],
 }
+/// Represents the redeemed state of the swap, where the redeemer has withdrawn funds from the vault
 #[event]
 pub struct Redeemed {
     pub initiator: Pubkey,
     pub secret: [u8; 32],
 }
+/// Represents the refund state of the swap, where the initiator has withdrawn funds from the vault past expiry
 #[event]
 pub struct Refunded {
     pub initiator: Pubkey,
     pub secret_hash: [u8; 32],
 }
+/// Represents the instant refund state of the swap, where the initiator has withdrawn funds the vault
+/// with the redeemer's consent
 #[event]
 pub struct InstantRefunded {
     pub initiator: Pubkey,
@@ -185,13 +235,13 @@ pub struct InstantRefunded {
 
 #[error_code]
 pub enum SwapError {
-    #[msg("The provided initiator is not the original initiator of this swap account")]
+    #[msg("The provided initiator is not the original initiator of this swap")]
     InvalidInitiator,
 
-    #[msg("The provided redeemer is not the original redeemer of this swap amount")]
+    #[msg("The provided redeemer is not the original redeemer of this swap")]
     InvalidRedeemer,
 
-    #[msg("The provided secret does not correspond to the secret hash in the swap account")]
+    #[msg("The provided secret does not correspond to the secret hash of this swap")]
     InvalidSecret,
 
     #[msg("Attempt to perform a refund before expiry time")]
